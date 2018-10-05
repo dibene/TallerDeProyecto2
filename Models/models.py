90fe9f3fe52bce03fb35a46d1ddf7696524bbab9
Leandro Cuvelo <lea.cuvelo@gmail.com>
2018-10-05 20:33:15 -0300

    Added Location model

---
+++ Models/models.py
@@ -0,0 +1,26 @@
+import datetime
+
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+#This models will be used in our DataBase.
+#And contains all the data to show in the html.
+class Locations(Base):
+    __tablename__ = 'locations'
+    id=Column(Integer, primary_key=True)
+    longitude=Column('longitude', float)
+    latitude=Column('latitude', float)
+
+    def serialize(self):	#This function returns the objects data in a easily serializable format.
+    	return{
+    		'id': self.id,
+    		'longitude': self.longitude,
+    		'latitude': self.latitude,
+    	}
+    
+
+
