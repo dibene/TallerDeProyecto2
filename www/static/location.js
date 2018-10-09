3209f72e41fb24eb71b93222c039ab8ae432bf5b
Leandro Cuvelo <lea.cuvelo@gmail.com>
2018-10-09 14:31:40 -0300

    Added new API Key

--- www/static/location.js
+++ www/static/location.js
@@ -1,33 +1,7 @@
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.9205233, lng: -57.9881898},
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 43.5293101, lng: -5.6773233},
     zoom: 13
-});
-var marker = new google.maps.Marker({
-    position: {lat: -34.9205233, lng: -57.9881898},
-    map: map,
-    title: 'COMBI I'
-});
-
-}
-
-$(document).ready(function(){
-    sec = 5000;
-    intervalId=0;
-    function sendRequest(){
-        $.ajax({
-          url: "/last",
-          success: 
-            function(data){
-                // TODO graficar en el mapa
-                $( "#divFooter" ).append("<p>latitude:"+ data.latitude +" , longitude: "+data.longitude+" time: "+data.time+"</p>");
-          },
-          complete: function() {
-            clearInterval(intervalId);
-            intervalId = setInterval(sendRequest, sec); 
-       }
-      });
-    };
-    sendRequest();
   });
+}
