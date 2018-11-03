26ac3a71bb8f2a31e792ffdb3aee537ca8701f83
Leandro Cuvelo <lea.cuvelo@gmail.com>
2018-11-03 18:19:02 -0300

    Added validation in Datetime to avoid sql exceptions

--- www/app.py
+++ www/app.py
@@ -1,4 +1,3 @@
-# Imports
 from flask import Flask
 from flask import render_template
 from database import Database
@@ -7,6 +6,7 @@ from flask import request
 
 import subprocess
 import os
+import time
 
 app = Flask(__name__)
 db = Database()
@@ -24,12 +24,30 @@ def get_last_location():
 @app.route('/writesample', methods=['POST'])
 def writeLocation():
     if request.method == 'POST':
-        if request.form['latitude'] and request.form['longitude'] and request.form['datetime']:
-            db.addSample(latitude = request.form['latitude'], longitude = request.form['longitude'], datetime = request.form['datetime'])
-            return "OK "+request.form['latitude']+" "+request.form['longitude']+" "+request.form['datetime']
+        if isValidForm(request):
+            try:
+                correctDateTimeFormatted = isCorrectDatetimeWithReturn(request.form['datetime'])
+                if(correctDateTimeFormatted != "Error datetime"):
+                    db.addSample(latitude = request.form['latitude'], longitude = request.form['longitude'], datetime = correctDateTimeFormatted)
+                    return "OK "+request.form['latitude']+" "+request.form['longitude']+" "+request.form['datetime']
+                else:
+                    return "Error in Datetime, must be YYYY-MM-DD HH:mm:ss"
+            except ValueError:
+                print("Incorrect date!")
     else:
         return "ERROR"
-        
+
+def isValidForm(request):
+    return request.form['latitude'] and request.form['longitude'] and request.form['datetime']
+
+def isCorrectDatetimeWithReturn(datetimeToValidate):
+    timeformat = "%Y-%m-%d %H:%M:%S"
+    try:
+        time.strptime(datetimeToValidate, timeformat)
+        return datetimeToValidate
+    except ValueError:
+        return "Error datetime"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888)
 
