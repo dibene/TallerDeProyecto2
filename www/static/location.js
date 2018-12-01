6682457c1fdfc9ac1907ad481e038b016883253d
Santiago Dibene <dibenesantiago@gmail.com>
2018-12-01 12:36:31 -0300

    Auto stash before merge of "master" and "origin/master"

--- www/static/location.js
+++ www/static/location.js
@@ -5,15 +5,32 @@ var sec = 15000;
 var intervalId = 0;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.9205233, lng: -57.9881898},
-    zoom: 13
-});
-marker = new google.maps.Marker({
-    position: {lat: -34.9205233, lng: -57.9881898},
-    map: map,
-    title: 'COMBI I'
-});
+  $.ajax({
+    url: "/last",
+    type: 'get',
+    success: function (data, status, jqXHR) {
+      var iconBase = '/static/img/'; 
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: data.latitude, lng: data.longitude},
+        zoom: 15
+      });
+    
+      marker = new google.maps.Marker({
+        position: {lat: data.latitude, lng:  data.longitude},
+        icon: iconBase + 'bus_aHy_icon.ico',
+        map: map,
+        title: 'COMBI I'
+      });
+    },
+    error: function (msg, status, jqXHR) {
+      console.log("ERROR");
+      console.log("GET LAST LOCATION ERROR --> " + msg);
+    },
+    complete: function(){
+      clearInterval(intervalId);
+      intervalId = setInterval(displayLastLocation, sec); 
+    }
+  });
 
 }
 
@@ -23,6 +40,7 @@ function displayLastLocation(){
     type: 'get',
     success: function (data, status, jqXHR) {
       lastLatLng = new google.maps.LatLng(data.latitude, data.longitude);
+      map.setCenter(lastLatLng);
       marker.setPosition(lastLatLng);
       console.log("SUCCESS");
       console.log("SUCCES TO RETRIEVE DATA Latitude: " + data.latitude);
